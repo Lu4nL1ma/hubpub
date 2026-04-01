fbff561658d35c7df9269a9b02b6b85520902041
Lu4nL1ma <luanlima.corp@gmail.com>
2026-04-01 18:36:13 +0000

    stgs_13

--- hubpub/hubpub/settings.py
+++ hubpub/hubpub/settings.py
@@ -73,7 +73,7 @@ USE_TZ = True
 
 # 7. ARQUIVOS ESTÁTICOS E MÍDIA
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 
 MEDIA_URL = '/media/'
