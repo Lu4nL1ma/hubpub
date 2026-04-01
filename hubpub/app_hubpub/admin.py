0a85d3932431dd34eb6522afe4574b6205bedf3d
Lu4nL1ma <luanlima.corp@gmail.com>
2026-04-01 19:36:25 +0000

    admin_19

--- hubpub/app_hubpub/admin.py
+++ hubpub/app_hubpub/admin.py
@@ -4,10 +4,10 @@ from .models import divulgacao_agend
 @admin.register(divulgacao_agend)
 class DivulgacaoAgendAdmin(admin.ModelAdmin):
     # Colunas que aparecerão na lista do Admin
-    list_display = ('id','rede_social', 'tipo_post', 'data_criacao', 'legenda','hora', 'ultima_publicacao')
+    list_display = ('id','rede_social', 'tipo_post', 'data', 'legenda','hora', 'ultima_publicacao')
     
     # Filtros na lateral direita
-    list_filter = ('id','rede_social', 'tipo_post', 'data_criacao', 'legenda','ultima_publicacao')
+    list_filter = ('id','rede_social', 'tipo_post', 'data', 'legenda','ultima_publicacao')
     
     # Campo de busca (útil quando tiver muitos registros)
     search_fields = ('rede_social', 'legenda')
