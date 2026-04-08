34685d6eabffc93e7f4e247a041c18d590a63f13
Lu4nL1ma <luanlima.corp@gmail.com>
2026-04-08 20:56:07 +0000

    cntrl_91

--- hubpub/hubpub/urls.py
+++ hubpub/hubpub/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from app_hubpub.views import home, staff, agenda, form_agenda, listar_cursos, cadastrar_curso, detalhe_curso, inserir_aluno, excluir_aluno
+from app_hubpub.views import home, staff, agenda, form_agenda, listar_cursos, cadastrar_curso, detalhe_curso, inserir_aluno, excluir_aluno, controle_presenca
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
